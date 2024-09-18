7133bdc449d8e79253ee5cbfd4850adf9c9ffeae
BeepBeep62 <samiaakhtar17@gmail.com>
2024-09-18 19:12:29 +0500

    Situation uiz
    WITH BELLA THE WOLF?????!??!?!!

--- script.py
+++ script.py
@@ -1,3 +1,14 @@
-print("hello world!") #this prints a message
-print(6+1)
-print("coding"+"al")
+print("HI! TIME FOR A DELICOUS GORMET MEAL!!!")
+print("YOU ARE STUCK IN A ROOM WITH BELLA THE WOLF AND SHE'S ABOUT TO ILLEGALLY ASSAULT YOU")
+print("1. throw your sister's crib at her and permanently give her hemorroids")
+print("2. turn into 3D spinning taurus")
+print("3. create grooming allegations about her")
+answer = input("whadda u do: ")
+if answer== 1:
+ print("congrats sauzlations! you have permanently traumatized bella the wolddff!")
+elif answer== 2:
+ print("you have killed bella with your confusion tactiscts")
+elif answer== 3:
+ print("you haved DESTROYED BELLA")
+else:
+ print("....well atleeast yougot THE HOLIGONA!! MA SHEEPOWTEEJ AY GOOL OSTAMA HOOMAYOWA THOOGEELAZY!!")
