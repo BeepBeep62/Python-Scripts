a496061e2dd6d8bcee82363edb4e736255421cf4
BeepBeep62 <samiaakhter17@gmail.com>
2024-09-21 12:50:25 +0500

    typecasting lesson

---
+++ typecastingh.py
@@ -0,0 +1,17 @@
+# £convert float to integer, string to integer, integer to float, integer to string.
+A = 8.6
+print("A's data:",type(A))
+B = int (A)
+print(type(B))
+A2 = "1031"
+print(type(A2))
+A2 = int(A2)
+print(type(A2))
+itt = 4
+print(type(itt))
+itt = float(itt)
+print(type(itt),itt)
+itrag = 6
+print(type(itrag),itrag)
+itrag = str(itrag)
+print(type(itrag),itrag)
