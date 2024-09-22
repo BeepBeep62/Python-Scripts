5f976e9db9f406d7fb4adc3b9937fb4fa20d1583
BeepBeep62 <samiaakhter17@gmail.com>
2024-09-22 13:08:41 +0500

    precentagded

---
+++ Percentage.py
@@ -0,0 +1,8 @@
+# Raj scored 40, 70, 50 and 60 out of 100 in maths, science, Hindi and English. Find the percentage he got?
+math = 40
+science = 70
+urdu = 60
+English = 50
+sumt = math + science + urdu + English
+percentage = (sumt/400)*100
+print(percentage)
