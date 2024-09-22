c9537332ceacbb9b33f1f8f8214a7703a69e88d9
BeepBeep62 <samiaakhter17@gmail.com>
2024-09-22 12:35:34 +0500

    this calculates an average number of many

---
+++ Average.py
@@ -0,0 +1,11 @@
+# There are five trees in Jack's front yard. He checks each tree to find out how tall it is in inches and writes the height on a sheet of paper. Jack's list: 98, 94, 41, 96, and 11. What is the average height of a tree in Jack's front yard?
+tree1 = 98
+tree2 = 94
+tree3 = 41
+tree4 = 96
+tree5 = 11
+tree6 = 24
+# we will be calculating the average number with these numbers, average = added every height divided by total number of the trees
+sumheight = tree1 + tree2 + tree3 + tree4 + tree5 + tree6
+average = sumheight//6
+print("average height of tree is",average)
