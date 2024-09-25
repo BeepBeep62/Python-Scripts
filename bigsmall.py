bcc4877618ce1f513075406264c965864f14c0bd
BeepBeep62 <samiaakhter17@gmail.com>
2024-09-25 17:18:15 +0500

    odd even and stuff

---
+++ bigsmall.py
@@ -0,0 +1,5 @@
+num = int(input("enter number: "))
+if num >50:
+ print("the number",num,"is greater than 50")
+else:
+ print("the number",num,"is less than 50")
---
+++ oddeven.py
@@ -0,0 +1,5 @@
+num = int(input("enter something NOWW!!:"))
+if num%2==0:
+ print("even")
+else:
+ print("odd")
