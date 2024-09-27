4e578c905e18d23dbbe85da5b0ab839e1f076cd1
BeepBeep62 <samiaakhter17@gmail.com>
2024-09-27 15:50:38 +0500

    BOOLEAN TEST

---
+++ NOT.py
@@ -0,0 +1,6 @@
+string = input("enter something and check if it matches the word: ")
+word = "Codingal"
+if string != word:
+ print("your input doesnt match the word")
+if string == word:
+ print("your input matches the word")
