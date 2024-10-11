737081086c00f0fe45b0a59c4aa4e9c7ec27bbb3
BeepBeep62 <samiaakhter17@gmail.com>
2024-10-11 16:38:51 +0500

    ok

---
+++ square.py
@@ -0,0 +1,12 @@
+import turtle
+turtle.Screen().setup(420,380)
+turtle.Screen().bgcolor("purple")
+Pen = turtle.Turtle()
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(90)
+turtle.done()
