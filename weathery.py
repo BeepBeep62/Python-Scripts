a100aee1ca2faf01335251afcb261692b06fd09d
BeepBeep62 <samiaakhter17@gmail.com>
2024-10-12 12:45:53 +0500

    YES OK

---
+++ weathery.py
@@ -0,0 +1,7 @@
+# Write a program to display the weather in autumn and spring
+def weatha():
+    print("weather in autumn is s,",auttym)
+    print("weather in spring is",sping)
+auttym = "its windy"
+sping = "its oven buteiful"
+weatha()
