7f40a2086e6a4845fb6a2eeb29676e4070e95aff
BeepBeep62 <samiaakhter17@gmail.com>
2024-11-24 15:13:06 +0500

    all

---
+++ classic.py
@@ -0,0 +1,19 @@
+# Write a program to create a class Parrot and perform the following tasks - 1. Create a class variable species 2. Create a __init__ method that has instance variables - name and age 3. Create instances of class Parrot, passing arguments as well 4. Print Class variable by accessing it 5. Print Instance variables as well
+class parrot:
+    # you are creATING a clASS vArIaBlele
+    species = "BIRD"
+    # eve this is a instnv ce varibaele
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+# initioate memes you are creating an parrote initiatijgn parrot class= cre
+caramel = parrot(name="caramel", age=0.038)
+print(parrot.species)
+print("yes")
+
+
+
+
+
+
+    
